fb478143e14831be04118aaae1df31a08cff0c27
Abhinav <161115308+Abhinavbyte-ui@users.noreply.github.com>
2025-06-28 10:27:49 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "abhinav@gmail.com"
+var accountPassword = "1234"
+account = "Jaipur"
+
+// accountId = 2   not allowed
+
+accountEmail =" abhi@123"
+accountPassword ="9931"
+accountId =" Bengaluru"
+console.log(accountId);
+
+console.table([ accountEmail, accountPassword, accountId, accountId])
