69d2db6e572ac3b31d9335b559ee67ec02272943
Abhinav <161115308+Abhinavbyte-ui@users.noreply.github.com>
2025-06-28 11:39:59 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use Strict"; // treat all Js code ass newer version
+
+//alert(3+3) //we are using node.js, not browser
+
+let name = "abhinav"
+let age =  18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+    // symbol => unique
+
+// object
+
+console.log(typeof null)
